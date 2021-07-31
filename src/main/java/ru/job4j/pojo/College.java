78656e0f3a153e06a33e5bcb13b083c94b92a954
Dmitry Sedikov <sagkar.savaie@yandex.ru>
2021-07-31 20:11:12 +0300

    2. Модель данных. [#174683 #183869]

--- src/main/java/ru/job4j/pojo/College.java
+++ src/main/java/ru/job4j/pojo/College.java
@@ -6,8 +6,8 @@ public class College {
         student.setName("Ivanov Ivan");
         student.setAdmission("2000-10-10");
         student.setGroup("PM2");
-        System.out.print("Student: " + student.getName() + "\n"
-                + "Date of admission: " + student.getAdmission() + "\n"
+        System.out.print("Student: " + student.getName() + System.lineSeparator()
+                + "Date of admission: " + student.getAdmission() + System.lineSeparator()
                 + "Group: " + student.getGroup());
     }
 }
