c8865c46afeca09c4630a817e3a233b785fb74bd
Dmitry Sedikov <sagkar.savaie@yandex.ru>
2021-07-31 06:32:35 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -21,5 +23,35 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findAll() {
+        Item[] itemsWithoutNull = new Item[items.length];
+        for (Item value : items) {
+            if (value != null)
+                itemsWithoutNull[size] = value;
+            size++;
+        }
+        itemsWithoutNull = Arrays.copyOf(itemsWithoutNull, size);
+        for (Item item : itemsWithoutNull) {
+            System.out.println(item);
+        }
+        return items;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] withoutNull = new Item[items.length];
+        int size = 0;
+        for (Item value : items) {
+            if (value != null && value.getName().equals(key)) {
+                withoutNull[size] = value;
+                size++;
+            }
+        }
+        withoutNull = Arrays.copyOf(withoutNull, size);
+        for (Item item : withoutNull) {
+            System.out.println(item);
+        }
+        return withoutNull;
     }
 }
