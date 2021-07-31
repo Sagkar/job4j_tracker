ebe273364c9f47686cab896e0f0bd1173dda2971
Dmitry Sedikov <sagkar.savaie@yandex.ru>
2021-07-31 05:04:15 +0300

    3. Массивы и модели. [#174892]

---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,25 @@
+package ru.job4j.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book cleanCode = new Book("Clean Code", 500);
+        Book cook = new Book("Cooking", 300);
+        Book math = new Book("Math", 450);
+        Book anatomy = new Book("Anatomy", 350);
+        Book[] books = {cleanCode,cook,math,anatomy};
+        for (Book book : books) {
+            System.out.println(book.getName() + " " + book.getPages() + " pages");
+        }
+        Book temp = books[0];
+        books[0] = books[3];
+        books[3] = temp;
+        for (Book book: books) {
+            System.out.println(book.getName() + " " + book.getPages() + " pages");
+        }
+        for (Book book : books) {
+            if (book.getName().equals("Clean Code")) {
+                System.out.println(book.getName() + " " + book.getPages() + " pages");
+            }
+        }
+    }
+}
