977fd35846c1e49b4124a16591ed965fb06a6a50
Dmitry Sedikov <sagkar.savaie@yandex.ru>
2021-08-01 05:00:17 +0300

    4. Переопределение [#174890 #183864]

--- src/main/java/ru/job4j/inheritance/JsonReport.java
+++ src/main/java/ru/job4j/inheritance/JsonReport.java
@@ -3,7 +3,9 @@ package ru.job4j.inheritance;
 public class JsonReport extends TextReport {
     @Override
     public String generate(String name, String body) {
-        return   name + ": name \n"
-                + body + ": body";
+        return  "{" + System.lineSeparator()
+                + name + ": name," + System.lineSeparator()
+                + body + ": body" +System.lineSeparator() +
+                "}";
     }
 }
