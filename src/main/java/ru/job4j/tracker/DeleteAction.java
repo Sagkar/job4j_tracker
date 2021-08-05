ba7b1e560e3c7882f16d3d0196fcfbc2b4cc246d
Dmitry Sedikov <sagkar.savaie@yandex.ru>
2021-08-05 12:16:40 +0300

    4.1. Разрыв зависимости StartUI от Scanner. [#181778 #186132]

--- src/main/java/ru/job4j/tracker/DeleteAction.java
+++ src/main/java/ru/job4j/tracker/DeleteAction.java
@@ -15,7 +15,7 @@ public class DeleteAction implements UserAction {
     @Override
     public boolean execute(Input input, Tracker tracker) {
         output.println("=== Delete item ====");
-        int id = Integer.parseInt(input.askStr("Enter id: "));
+        int id = input.askInt("Enter id: ");
         if (tracker.delete(id)) {
             output.println("Заявка удалена успешно.");
         } else {
