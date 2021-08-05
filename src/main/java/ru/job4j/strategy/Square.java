b53f48e0bb479bb2c64ce5e84b67e7d53ad68419
Dmitry Sedikov <sagkar.savaie@yandex.ru>
2021-08-05 08:42:55 +0300

    Checkstyle fix

--- src/main/java/ru/job4j/strategy/Square.java
+++ src/main/java/ru/job4j/strategy/Square.java
@@ -1,13 +1,13 @@
 package ru.job4j.strategy;
 
-public class Square implements Shape{
+public class Square implements Shape {
     @Override
     public String draw() {
         String ln = System.lineSeparator();
         return    " __________" + ln
-                + "|          |"+ ln
-                + "|          |"+ ln
-                + "|          |"+ ln
+                + "|          |" + ln
+                + "|          |" + ln
+                + "|          |" + ln
                 + "|__________|";
     }
 }
