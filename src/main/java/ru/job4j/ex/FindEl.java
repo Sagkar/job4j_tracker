1e1d5cd0ca2f5cc949eda7688b54d1ba7cb050a2
Dmitry Sedikov <sagkar.savaie@yandex.ru>
2021-08-11 18:05:44 +0300

    0.4. Пользовательские исключения. [#219367 #188546]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -4,7 +4,7 @@ public class FindEl {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
         int rsl = -1;
         for (int i = 0; i < value.length; i++) {
-            if (value[i] ==  key) {
+            if (value[i].equals(key)) {
                 rsl = i;
                 return rsl;
             }
@@ -16,9 +16,9 @@ public class FindEl {
     }
 
     public static void main(String[] args) {
-        String[] array = new String[1];
+        String[] array = new String[] {"randomName"};
         try {
-            indexOf(array,"nullValue");
+            indexOf(array,"random");
         } catch (ElementNotFoundException e) {
             e.printStackTrace();
         }
