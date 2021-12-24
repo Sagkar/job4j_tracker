07c9981632709d6b1dba0e5b33eaca7d9d68826a
Dmitry Sedikov <sagkar.savaie@yandex.ru>
2021-12-24 18:01:08 +0300

    2. Комбинированный компаратор [#10036]

--- src/test/java/ru/job4j/collection/JobTest.java
+++ src/test/java/ru/job4j/collection/JobTest.java
@@ -78,11 +78,21 @@ public class JobTest {
     }
 
     @Test
-    public void whenCompatorAscByNameAndPrority() {
+    public void whenCompatorAscBySameNamesAndPrority() {
         Comparator<Job> cmpNamePriority = new JobAscByName().thenComparing(new JobAscByPriority());
         int rsl = cmpNamePriority.compare(
-                new Job("Impl task", 0),
-                new Job("Fix bug", 1)
+                new Job("Aaa", 0),
+                new Job("Aaa", 1)
+        );
+        assertThat(rsl, lessThan(0));
+    }
+
+    @Test
+    public void whenCompatorDescBySameNamesAndPrority() {
+        Comparator<Job> cmpNamePriority = new JobDescByName().thenComparing(new JobDescByPriority());
+        int rsl = cmpNamePriority.compare(
+                new Job("Aaa", 0),
+                new Job("Aaa", 1)
         );
         assertThat(rsl, greaterThan(0));
     }
