3c9107a337bc21a163845b67b7ffc094abe7b98d
Sagkar <Sagkar.savaie@yandex.ru>
2022-02-16 12:59:37 +0300

    0. Stream API улучшения [#58167]

--- src/main/java/ru/job4j/stream/Student.java
+++ src/main/java/ru/job4j/stream/Student.java
@@ -11,6 +11,10 @@ public class Student {
         this.score = score;
         this.surname = surname;
     }
+    public Student(String surname, int score) {
+        this.score = score;
+        this.surname = surname;
+    }
 
     public int getScore() {
         return score;
