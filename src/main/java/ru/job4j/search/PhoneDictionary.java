dbc96eacd17ebcf7ae2181353e8664bfa11d45c9
Sagkar <Sagkar.savaie@yandex.ru>
2022-02-17 12:32:22 +0300

    2. Local-Variable Type Inference [#70626 #265336]

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -22,7 +22,7 @@ public class PhoneDictionary {
         Predicate<Person> byAddress = person -> person.getAddress().contains(key);
         Predicate<Person> combine = byName.or(bySurname).or(byPhone).or(byAddress);
         var result = new ArrayList<Person>();
-        for (Person person : persons) {
+        for (var person : persons) {
             if (combine.test(person)) {
                 result.add(person);
             }
