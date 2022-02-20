0bcfdaf14e19af2cb4930a60ca0bda5c9035c538
Sagkar <Sagkar.savaie@yandex.ru>
2022-02-20 15:22:13 +0300

    2. Аттестация [#49777 #266722]

--- src/main/java/ru/job4j/stream/Analyze.java
+++ src/main/java/ru/job4j/stream/Analyze.java
@@ -75,7 +75,6 @@ public class Analyze {
         return stream.flatMap(pupil -> pupil.getSubjects().stream())
                 .collect(Collectors.groupingBy(
                         Subject::getName,
-                        LinkedHashMap::new,
                         Collectors.summingDouble(Subject::getScore)))
                 .entrySet()
                 .stream()
